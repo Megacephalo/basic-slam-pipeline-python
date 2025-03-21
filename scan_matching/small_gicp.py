7473f2c6f14f6ac5c9de1029219b1b13a376783a
Charly Huang <charly.charlongo@gmail.com>
2025-03-21 00:04:10 -0700

    Remove commented out code lines to improve clarity and readability

--- scan_matching/small_gicp.py
+++ scan_matching/small_gicp.py
@@ -13,16 +13,7 @@ class Small_gicp(Scan_matcher_interface):
     def __init__(self):
         self._numThreads: int = NUM_THREADS
         self._voxel_size: float = VOXEL_SIZE
-        # self._registration_type: str = 'VGICP' # Options: 'GICP', 'VGICP'
-        # self._resolution: float = 1.0
-        # self._max_correspondence_distance: float = 0.5
-        # self._max_iterations: int = 200
-        # self._source_cloud: np.ndarray = None
-        # self._target_cloud: np.ndarray = None
-        # self._T_world_lidar: np.ndarray = np.identity(4)
-        # self._is_init: bool = False
 
-        # For the second type of align()
         self._T_last_current: np.ndarray = np.identity(4)
         self._T_world_lidar: np.ndarray = np.identity(4)
         self._target_state: Tuple[np.ndarray, small_gicp.KdTree] = None
@@ -32,35 +23,6 @@ class Small_gicp(Scan_matcher_interface):
     
     def set_target(self, target)->None:
         self._target_cloud = target[:, :3]
-
-
-    # def estimate(self)->np.ndarray:
-    #     assert self._source_cloud is not None, 'Source point cloud is not set'
-    #     assert self._target_cloud is not None, 'Target point cloud is not set'
-
-    #     if not self._is_init:
-    #         self._is_init = True
-    #         return self._T_world_lidar
-
-    #     result = small_gicp.align(
-    #         target_points=self._target_cloud,
-    #         source_points=self._source_cloud,
-    #         init_T_target_source=self._T_world_lidar,
-    #         registration_type=self._registration_type,
-    #         voxel_resolution=self._resolution,
-    #         max_correspondence_distance=self._max_correspondence_distance,
-    #         max_iterations=self._max_iterations,
-    #         num_threads=self._numThreads
-    #     )
-
-    #     # DEBUG
-    #     # print(f'Result:\n{result}')
-    #     print(result.num_inliers)
-    #     # DEBUG
-
-    #     self._T_world_lidar = self._T_world_lidar @ result.T_target_source
-
-    #     return self._T_world_lidar
     
 
     def estimate(self)->np.ndarray:
@@ -77,10 +39,6 @@ class Small_gicp(Scan_matcher_interface):
             init_T_target_source=self._T_last_current,
             num_threads=self._numThreads
         )
-
-        # DEBUG
-        # print(f'Inliers: {result.num_inliers}')
-        # DEBUG
 
         self._T_last_current = result.T_target_source
         self._T_world_lidar = self._T_world_lidar @ result.T_target_source
